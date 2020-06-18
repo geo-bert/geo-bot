318fb51bb99d6c6060d503eed1b75737381f0d74
geo-bert <grybl.business@gmail.com>
2020-06-18 17:15:21 +0200

    v1.0.5

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = 'NzIyMTMzNDE5MTE0NjI3MjAz.XueqlQ.h-4pt4Kbk2voAt_5k0MXwUyQaH8';
 
-bot.on('ready', () =>{
-    console.log('This bot is online!');
-    bot.user.setActivity('over link previews', {type: 'WATCHING'});
-});
-
 var allowed = [
     "youtube.com/watch",
     "youtu.be",
@@ -14,16 +9,10 @@ var allowed = [
     "imgur.com"
 ];
 
-bot.on('message', msg =>{
-    for(let i = 0; i < allowed.length; i++){
-        if(msg.content.includes(allowed[i])){
-            return;
-        }
-    }
-    msg.suppressEmbeds(true);
-});
-
 bot.on('ready', () =>{
+    console.log('This bot is online!');
+    bot.user.setActivity('over link previews', {type: 'WATCHING'});
+    
     function scheduledMessage(){
         bot.channels.cache.get('303288155124269056').send('noob')
             .then(sentMessage => {
@@ -33,6 +22,15 @@ bot.on('ready', () =>{
     }
 
     setInterval(scheduledMessage, 300000);
+});
+
+bot.on('message', msg =>{
+    for(let i = 0; i < allowed.length; i++){
+        if(msg.content.includes(allowed[i])){
+            return;
+        }
+    }
+    msg.suppressEmbeds(true);
 });
 
 bot.login(token);
