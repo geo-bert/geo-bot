c99e390f5b74efaf136fc561a6f8ed002c61b0a5
geo-bert <grybl.business@gmail.com>
2020-11-18 18:52:41 +0100

    Change all embeds are now blocked

--- index.js
+++ index.js
@@ -2,32 +2,14 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = 'NzIyMTMzNDE5MTE0NjI3MjAz.XuepJw.xJl73SmUfVsG0JmofhU71y25LFI';
 
-var allowed = [
-    "youtube.com/watch",
-    "youtu.be",
-    "redd.it",
-    "imgur.com"
-];
-
 bot.on('ready', () => {
     console.log('This bot is online!');
     bot.user.setActivity('over link previews', { type: 'WATCHING' });
 });
 
 bot.on('message', msg => {
-    if (msg.channel == "547000766779490304" && msg.embeds.length > 0) {
-        msg.suppressEmbeds(true)
-            .then(() => console.log(`Supressing message by: ${msg.author.username}`))
-            .catch(console.error);
-
-        for (let i = 0; i < allowed.length; i++) {
-            if (msg.content.includes(allowed[i])) {
-                msg.suppressEmbeds(false)
-                    .then(() => console.log(`Message by ${msg.author.username} cleared`))
-                    .catch(console.error);;
-            }
-        }
-
+    if (msg.channel == "547000766779490304") {
+        msg.suppressEmbeds(true).then().catch(console.error);
     }
 });
 
