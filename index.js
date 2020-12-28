c9f6b546753638c6e0a47a904be26d9988a5cfea
geo-bert <grybl.business@gmail.com>
2020-12-28 21:59:33 +0100

    pleasing the peasants

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const token = 'NzIyMTMzNDE5MTE0NjI3MjAz.XuepJw.xJl73SmUfVsG0JmofhU71y25LFI';
 
+var allowed = [
+    "youtube.com/watch",
+    "youtu.be",
+    "redd.it",
+    "imgur.com"
+];
+
 bot.on('ready', () => {
     console.log('This bot is online!');
     bot.user.setActivity('over link previews', { type: 'WATCHING' });
@@ -9,6 +16,11 @@ bot.on('ready', () => {
 
 bot.on('message', msg => {
     if (msg.channel == "547000766779490304" || msg.channel == "784923413663580160") {
+        for(let i = 0; i < allowed.length; i++){
+            if(msg.content.includes(allowed[i])){
+                return;
+            }
+        }
         msg.suppressEmbeds(true).then().catch(console.error);
     }
 });
