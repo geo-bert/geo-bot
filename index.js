ebf2655e4a6f1de1f63944d5e076dbfc419c56ac
geo-bert <grybl.business@gmail.com>
2021-08-27 00:09:52 +0200

    Port dynamic channels

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import { Client, Intents } from "discord.js";
 import token from "./secrets.json";
 import checkForSuppression from "./src/suppress.js";
+import updateChannel from "./src/dynamic-channels.js";
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_VOICE_STATES,
+  ],
 });
 
 client.once("ready", () => {
@@ -12,5 +17,9 @@ client.once("ready", () => {
 });
 
 client.on("messageCreate", (msg) => checkForSuppression(msg));
+
+client.on("voiceStateUpdate", (oldState, newState) =>
+  updateChannel(oldState, newState)
+);
 
 client.login(token);
