b22f7fd1124521509cdb69a43c6fb1861da9012a
geo-bert <grybl.business@gmail.com>
2021-09-08 18:47:03 +0200

    Add autorole and leave-message

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ import { Client, Intents } from "discord.js";
 import token from "./secrets.json";
 import checkForSuppression from "./src/suppress.js";
 import updateChannel from "./src/dynamic-channels.js";
+import onLeave from "./src/leave-message.js";
+import autorole from "./src/autorole.js";
 
 const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_MESSAGES,
     Intents.FLAGS.GUILD_VOICE_STATES,
+    Intents.FLAGS.GUILD_MEMBERS,
   ],
 });
 
@@ -21,5 +24,9 @@ client.on("messageCreate", (msg) => checkForSuppression(msg));
 client.on("voiceStateUpdate", (oldState, newState) =>
   updateChannel(oldState, newState)
 );
+
+client.on("guildMemberAdd", (member) => autorole(client, member));
+
+client.on("guildMemberRemove", (member) => onLeave(client, member));
 
 client.login(token);
---
+++ src/leave-message.js
@@ -0,0 +1,8 @@
+import config from "../config.json";
+
+export default function onLeave(client, member) {
+  const channel = client.guilds.cache
+    .get(config.server_id)
+    .channels.cache.get(config.leave_channel);
+  channel.send(`Goodbye, ${member.toString()}!`);
+}
