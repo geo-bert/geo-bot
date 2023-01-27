f9f490db654cfe9e78b0d668f12b5d8fd61d456e
geo-bert <grybl.business@gmail.com>
2023-01-27 17:36:44 +0100

    Add proper intent for embed suppression

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-import { ActivityType, Client, GatewayIntentBits } from "discord.js";
-import token from "./secrets.json" assert { type: "json" };
+import {ActivityType, Client, GatewayIntentBits} from "discord.js";
+import token from "./secrets.json" assert {type: "json"};
 import checkForSuppression from "./src/suppress.js";
 import updateChannel from "./src/dynamic-channels.js";
 import onLeave from "./src/leave-message.js";
 import autorole from "./src/autorole.js";
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildMembers,
-  ],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildMembers,
+    ],
 });
 
 client.once("ready", () => {
-  client.user.setActivity("the sky and slacking off", {
-    type: ActivityType.Watching,
-  });
-  console.log(`This bot is online! ${new Date()}`);
+    client.user.setActivity("the sky and slacking off", {
+        type: ActivityType.Watching,
+    });
+    console.log(`This bot is online! ${new Date()}`);
 });
 
 client.on("messageCreate", (msg) => checkForSuppression(msg));
 
 client.on("voiceStateUpdate", (oldState, newState) =>
-  updateChannel(oldState, newState)
+    updateChannel(oldState, newState)
 );
 
 client.on("guildMemberAdd", (member) => autorole(client, member));
