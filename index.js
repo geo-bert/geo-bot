92779eac30ed8d3b310662924d3cfec46be93b3a
geo-bert <grybl.business@gmail.com>
2023-01-27 17:17:45 +0100

    Removing vanity role commands

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import checkForSuppression from "./src/suppress.js";
 import updateChannel from "./src/dynamic-channels.js";
 import onLeave from "./src/leave-message.js";
 import autorole from "./src/autorole.js";
-import { initCommands, handleCommand } from "./src/commands.js";
 
 const client = new Client({
   intents: [
@@ -20,8 +19,6 @@ client.once("ready", () => {
     type: ActivityType.Watching,
   });
   console.log(`This bot is online! ${new Date()}`);
-
-  initCommands(client);
 });
 
 client.on("messageCreate", (msg) => checkForSuppression(msg));
@@ -33,9 +30,5 @@ client.on("voiceStateUpdate", (oldState, newState) =>
 client.on("guildMemberAdd", (member) => autorole(client, member));
 
 client.on("guildMemberRemove", (member) => onLeave(client, member));
-
-client.on("interactionCreate", async (interaction) =>
-  handleCommand(client, interaction)
-);
 
 client.login(token);
