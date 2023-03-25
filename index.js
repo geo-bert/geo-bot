5f0a896584a02f7d735acedec293b14d334ff677
Pasgru <pascal@gruber-net.com>
2023-03-25 12:34:05 +0100

    WIP channel locks

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-import { ActivityType, Client, GatewayIntentBits } from "discord.js";
+import {
+  ActivityType,
+  Client,
+  Collection,
+  GatewayIntentBits,
+  Events,
+} from "discord.js";
 import checkForSuppression from "./src/suppress.js";
 import updateChannel from "./src/dynamic-channels.js";
 import onLeave from "./src/leave-message.js";
 import autorole from "./src/autorole.js";
 import * as dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+import fs from "node:fs";
+import path from "node:path";
 dotenv.config();
 
 const client = new Client({
@@ -16,23 +29,70 @@ const client = new Client({
   ],
 });
 
-client.once("ready", () => {
+client.once(Events.ClientReady, () => {
   client.user.setActivity("the sky and slacking off", {
     type: ActivityType.Watching,
   });
   console.log(`This bot is online! ${new Date()}`);
 });
 
-client.on("messageUpdate", (_, msg) => checkForSuppression(msg));
+client.on(Events.MessageUpdate, (_, msg) => checkForSuppression(msg));
 
-client.on("messageCreate", (msg) => checkForSuppression(msg));
+client.on(Events.MessageCreate, (msg) => checkForSuppression(msg));
 
-client.on("voiceStateUpdate", (oldState, newState) =>
+client.on(Events.VoiceStateUpdate, (oldState, newState) =>
   updateChannel(oldState, newState)
 );
 
-client.on("guildMemberAdd", (member) => autorole(client, member));
+client.on(Events.GuildMemberAdd, (member) => autorole(client, member));
 
-client.on("guildMemberRemove", (member) => onLeave(client, member));
+client.on(Events.GuildMemberRemove, (member) => onLeave(client, member));
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, "src", "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = await import(filePath);
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ("data" in command && "execute" in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(
+      `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+    );
+  }
+}
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "There was an error while executing this command!",
+        ephemeral: true,
+      });
+    }
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
